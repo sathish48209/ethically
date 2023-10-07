2534cdf846f5dce5c9184d018d873c4f0c2ac7af
Sathish <sathish@Sathishs-MacBook-Air.local>
2023-10-07 11:44:21 +0530

    base codesetup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppBar from "./components/Appbar";
+import EmployeeDetails from "./components/admin-console/EmployeeDetails";
+import NoPage from "./pages/NoPage";
+import CompanyProfile from "./components/admin-console/CompanyProfile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppBar />}>
+          <Route index element={<EmployeeDetails />} />
+          <Route path="/companyprofile" element={<CompanyProfile />}></Route>
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
